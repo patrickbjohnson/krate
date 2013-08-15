b1ad92d9b3d45cee44e844e581ef3a93087cddd8
Patrick Johnson <patrickj@kratedesign.com>
2013-08-15 11:52:20 -0400

    final polish

--- config.rb
+++ config.rb
@@ -8,6 +8,9 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+output_style = :nested
+line_comments = false
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
